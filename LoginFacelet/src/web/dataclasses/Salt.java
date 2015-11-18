1a99677f9c1188608e3e3d30a0cc5187c9303954
BoBeni <maksim.ryzhov@frtk.ru>
2015-11-18 23:57:32 +0300

    +delete some mistakes after lesson

--- LoginFacelet/src/web/dataclasses/Salt.java
+++ LoginFacelet/src/web/dataclasses/Salt.java
@@ -4,7 +4,7 @@ import org.apache.commons.codec.digest.DigestUtils;
 
 public class Salt {
     public static void salting(User user) {
-        String password = DigestUtils.sha1Hex(user.getPassword()+user.getSault());
+        String password = DigestUtils.sha1Hex(user.getPassword() + user.getSault());
         user.setPassword(password);
     }
 }
